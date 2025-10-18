14b72d225e5178204c750584360607b7f18ca215
imranlatur24 <imranlatur24@gmail.com>
2025-10-18 16:36:15 +0530

    update in settings.py file

--- formvalidation_with__model/settings.py
+++ formvalidation_with__model/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-toww60*fwetc^ri)b$brb-9zrgg-)j-t+hjiyf__08k2y0yi%2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_beat',# for celery 
-    "crispy_forms",
-    "crispy_bootstrap5",
+    "crispy_forms",# bootstrap forms
+    "crispy_bootstrap5", # # bootstrap forms 2
     'api',
 ]
 
